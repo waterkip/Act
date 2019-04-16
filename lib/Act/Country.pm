a434760068940135a9b42cfb8961d699a7d91d5c
Harald Jörg <haj@posteo.de>
2019-04-16 09:09:39 +0200

    Eliminate 'en' from the list of country codes, because it isn't one 'en' is no country code.  It was added only recently, but breaks t/06country.t.

--- lib/Act/Country.pm
+++ lib/Act/Country.pm
@@ -10,7 +10,7 @@ my @COUNTRY_CODES = qw(
   ba bb bd be bf bg bh bi bj bl bm bn bo br bs bt bv bw by bz
   ca cc cd cf cg ch ci ck cl cm cn co cr cu cv cx cy cz
   de dj dk dm do dz
-  ec ee eg eh er es et en
+  ec ee eg eh er es et
   fi fj fk fm fo fr
   ga gb gd ge gf gg gh gi gl gm gn gp gq gr gs gt gu gw gy
   hk hm hn hr ht hu
